c0f7fc140fc9406ab252442bad8de8acd18d11d0
mjfrigaard <mjfrigaard@gmail.com>
2020-12-24 15:59:45 -0700

    new reactable tables

--- code/app.R
+++ code/app.R
@@ -1,89 +1,67 @@
 library(shiny)
 library(reactable)
 
-ui <- fluidPage(
+theme <- reactableTheme(borderColor = "#dfe2e5", stripedColor = "#f6f8fa",
+    highlightColor = "#f0f5f9", cellPadding = "8px 12px")
 
+ui <- fluidPage(
   titlePanel("Biotech stock prices"),
 
   reactableOutput(outputId = "table")
-  )
+)
 
 server <- function(input, output, session) {
-
-
-      output$plot <- renderPlot({
-        ggplot2::ggplot(data = iris, aes(x = Sepal.Length)) +
-            geom_histogram()
-    })
+  output$plot <- renderPlot({
+    ggplot2::ggplot(data = iris, aes(x = Sepal.Length)) +
+      geom_histogram()
+  })
 
   output$table <- renderReactable({
 
-# define reactable data input ---------------------------------------------
-    reactable(data = iris,
-          columnGroups = list(colGroup(name = "Sepal",
-                                       columns = c("Sepal.Length",
-                                                   "Sepal.Width")),
-                              colGroup(name = "Petal",
-                                       columns = c("Petal.Length",
-                                                   "Petal.Width"))),
-          filterable = TRUE,
-          searchable = TRUE,
-          resizable = TRUE,
-          showPageSizeOptions = TRUE,
-          onClick = "expand",
-          outlined = TRUE,
-          bordered = TRUE,
-          borderless = TRUE,
-          striped = TRUE,
-          highlight = TRUE,
-          compact = TRUE,
-          showSortable = TRUE,
-
-    defaultSorted = c("Sepal.Length", "Sepal.Width"),
-    columns = list(
-        Sepal.Length = colDef(name = "Length",
-                              aggregate = "max",
-                              format = colFormat(suffix = " cm",
-                                                 digits = 1)),
-        Sepal.Width = colDef(name = "Width",
-                             defaultSortOrder = "desc",
-                             aggregate = "mean",
-                             format = list(aggregated =
-                                               colFormat(suffix = " (avg)",
-                                                         digits = 2)),
-                                cell = function(value) {
-                                    if (value >= 3.3) {
-                                        classes <- "tag num-high"
-                                        } else if (value >= 3) {
-                                            classes <- "tag num-med"
-                                        } else {
-                                            classes <- "tag num-low"
-                                        }
-                                    value <- format(value, nsmall = 1)
-                                    span(class = classes, value)
-                                        },
-                                footer = function(values) {
-                                            div(tags$b("Average: "),
-                                                round(x = mean(values),
-                                                      digits = 1))
-                                                            }),
-        Petal.Length = colDef(name = "Length", aggregate = "sum"),
-        Petal.Width = colDef(name = "Width", aggregate = "count"),
-        Species = colDef(aggregate = "frequency")),
-            details = function(index) {
-                if (index == 3) {
-                    tabsetPanel(
-                        tabPanel(title = "plot",
-                                 plotOutput(outputId = "plot")),
-                        tabPanel(title = "subtable",
-                                 reactable(iris[1:3, 1:2],
-                                           fullWidth = FALSE)))
-                    } else if (index == 5) {
-                        paste("Details for row:", index)
-                    }
-                }
+    # define reactable data input ---------------------------------------------
+    reactable(iris,
+      filterable = TRUE, searchable = TRUE, sortable = FALSE,
+      showPageSizeOptions = TRUE, onClick = "expand", striped = TRUE,
+      columns = list(Sepal.Length = colDef(
+        name = "Sepal Length",
+        aggregate = "max", format = colFormat(
+          suffix = " cm",
+          digits = 1
+        )
+      ), Sepal.Width = colDef(
+        name = "Sepal Width",
+        defaultSortOrder = "desc", aggregate = "mean", format = list(aggregated = colFormat(
+          suffix = " (avg)",
+          digits = 2
+        )), cell = function(value) {
+          if (value >= 3.3) {
+            classes <- "tag num-high"
+          } else if (value >= 3) {
+            classes <- "tag num-med"
+          } else {
+            classes <- "tag num-low"
+          }
+          value <- format(value, nsmall = 1)
+          span(class = classes, value)
+        }
+      ), Petal.Length = colDef(
+        name = "Petal Length",
+        aggregate = "sum"
+      ), Petal.Width = colDef(
+        name = "Petal Width",
+        aggregate = "count"
+      ), Species = colDef(aggregate = "frequency")),
+      details = function(index) {
+        if (index == 3) {
+          tabsetPanel(
+            tabPanel("plot", plotOutput("plot")),
+            tabPanel("subtable", reactable(iris[1:3, 1:2], fullWidth = FALSE))
+          )
+        } else if (index == 5) {
+          paste("Details for row:", index)
+        }
+      }, theme = theme
     )
-
   })
 }
 
